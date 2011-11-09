0f93dcad664aa6bce515c63904384bb2c9b17356
AJ Christensen <aj@junglist.gen.nz>
2011-11-09 22:22:13 +1300

    0.2.0

--- cookbooks/pennyworth/metadata.rb
+++ cookbooks/pennyworth/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Configures the pennyworth continuous deployment pipeline system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "java"
 depends "jenkins"
