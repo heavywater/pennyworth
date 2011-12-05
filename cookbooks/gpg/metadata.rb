14b7ef42444c4064f45edb8f4572cac73d8208f1
AJ Christensen <aj@junglist.gen.nz>
2011-12-06 11:43:50 +1300

    #10 remove minitest references

--- cookbooks/gpg/metadata.rb
+++ cookbooks/gpg/metadata.rb
@@ -4,5 +4,3 @@ license          "Apache 2.0"
 description      "Installs/Configures gpg"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
-
-depends "minitest"
