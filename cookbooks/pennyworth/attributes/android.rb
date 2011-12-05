e7a4ad7f5d19f5acf22322762b8c514367ea2382
AJ Christensen <aj@junglist.gen.nz>
2011-12-06 11:58:19 +1300

    #4 android support, emulator bits

--- cookbooks/pennyworth/attributes/android.rb
+++ cookbooks/pennyworth/attributes/android.rb
@@ -1,2 +1,4 @@
 default[:pennyworth][:android_sdk_url] = "http://dl.google.com/android/android-sdk_r15-linux.tgz"
 default[:pennyworth][:android_path] = "/opt/android-sdk-linux"
+
+default[:pennyworth][:android][:avd] = "BBDdevice"
--- cookbooks/pennyworth/recipes/android.rb
+++ cookbooks/pennyworth/recipes/android.rb
@@ -1,3 +1,5 @@
+include_recipe "ant"
+
 node.default[:pennyworth][:android_sdk_tarfile] = File.join(Chef::Config[:file_cache_path],
                                                             URI.parse(node[:pennyworth][:android_sdk_url]).path.split("/").last)
 remote_file node[:pennyworth][:android_sdk_tarfile] do
@@ -5,8 +7,15 @@ remote_file node[:pennyworth][:android_sdk_tarfile] do
   action :create_if_missing
 end
 
+execute "android: directory permission" do
+  command "chown -R root:root #{node[:pennyworth][:android_path]}; " +
+    "chmod -R a+r #{node[:pennyworth][:android_path]}; " +
+    "find #{node[:pennyworth][:android_path]} -type d | xargs chmod 755"
+  action :nothing
+end
+
 execute "android: untar" do
-  command "tar zxvf #{node[:pennyworth][:android_sdk_tarfile]}"
+  command "tar zxf #{node[:pennyworth][:android_sdk_tarfile]}"
   cwd File.expand_path(File.join(node[:pennyworth][:android_path], ".."))
   creates node[:pennyworth][:android_path]
 end
@@ -21,4 +30,26 @@ execute "android: update sdk" do
   command "#{android_binary} update sdk -u -t platform,system-image,tool,platform-tool,source"
   path [ File.join(node[:pennyworth][:android_path], "tools") ]
   creates "/opt/android-sdk-linux/platform-tools"
+  notifies :run, "execute[android: directory permission]"
+end
+
+execute "android: create android virtual device (avd)" do
+  command "echo 'no' | #{android_binary} create avd " +
+    "--name #{node[:pennyworth][:android][:avd]} --target 3 --path /opt/avd --force"
+  creates "/opt/avd"
+end
+
+template "/etc/init/emulator.conf" do
+  source "emulator.conf.erb"
+  variables( :android_path => node[:pennyworth][:android_path],
+             :avd => node[:pennyworth][:android][:avd] )
+  mode "644"
+  notifies :restart, "service[android: emulator]"
+end
+
+service "android: emulator" do
+  service_name "emulator"
+  provider Chef::Provider::Service::Upstart
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
 end
